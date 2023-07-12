cced9957b871236132152d6fd644e00eee466901
RyanPurcell <ryan.purcell@comparepower.com>
2023-07-12 11:24:49 -0500

    fix(profiles): removed line breaks from reviews

--- src/composables/Profiles.js
+++ src/composables/Profiles.js
@@ -11,12 +11,7 @@ const profiles = ref([
         tech_level: 'Novice',
         interest: 'Clinical Psychology',
         rating: 5,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque felis orci, sed gravida nulla ' +
-            'molestie in. Ut nec sapien nunc. Quisque finibus finibus nulla in malesuada. Donec nec velit vulputate, ' +
-            'commodo ex et, consequat lacus. Maecenas vulputate, ipsum sit amet lobortis ornare, diam ex rhoncus nunc,nec ' +
-            'aliquam odio ante quis neque. Nunc ut nisl vel odio venenatis molestie eu vel sem. Aliquam vitae venenatis libero. ' +
-            'Etiam pharetra, elit eget tincidunt interdum, eros nisl mollis turpis,a ultrices turpis ligula sit amet leo. Aliquam ac ' +
-            'tincidunt nulla. Nullam scelerisque metus sit amet ipsum imperdiet, sit amet iaculis velit euismod.'
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque felis orci, sed gravida nulla molestie in. Ut nec sapien nunc. Quisque finibus finibus nulla in malesuada. Donec nec velit vulputate, commodo ex et, consequat lacus. Maecenas vulputate, ipsum sit amet lobortis ornare, diam ex rhoncus nunc,nec aliquam odio ante quis neque. Nunc ut nisl vel odio venenatis molestie eu vel sem. Aliquam vitae venenatis libero. Etiam pharetra, elit eget tincidunt interdum, eros nisl mollis turpis,a ultrices turpis ligula sit amet leo. Aliquam ac tincidunt nulla. Nullam scelerisque metus sit amet ipsum imperdiet, sit amet iaculis velit euismod.'
     },
     {
         first_name: 'Robert',
@@ -28,7 +23,12 @@ const profiles = ref([
         tech_level: 'Intermediate',
         rating: 4,
         interest: 'Support Services',
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque felis orci, sed gravida nulla molestie in. Ut nec sapien nunc. Quisque finibus finibus nulla in malesuada. Donec nec velit vulputate, commodo ex et, consequat lacus. Maecenas vulputate, ipsum sit amet lobortis ornare, diam ex rhoncus nunc,nec aliquam odio ante quis neque. Nunc ut nisl vel odio venenatis molestie eu vel sem. Aliquam vitae venenatis libero. Etiam pharetra, elit eget tincidunt interdum, eros nisl mollis turpis,a ultrices turpis ligula sit amet leo. Aliquam ac tincidunt nulla. Nullam scelerisque metus sit amet ipsum imperdiet, sit amet iaculis velit euismod.'
+        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque felis orci, sed gravida nulla 
+        molestie in. Ut nec sapien nunc. Quisque finibus finibus nulla in malesuada. Donec nec velit vulputate, commodo 
+        ex et, consequat lacus. Maecenas vulputate, ipsum sit amet lobortis ornare, diam ex rhoncus nunc,nec aliquam 
+        odio ante quis neque. Nunc ut nisl vel odio venenatis molestie eu vel sem. Aliquam vitae venenatis libero. 
+        Etiam pharetra, elit eget tincidunt interdum, eros nisl mollis turpis,a ultrices turpis ligula sit amet leo. 
+        Aliquam ac tincidunt nulla. Nullam scelerisque metus sit amet ipsum imperdiet, sit amet iaculis velit euismod.`
     },
     {
         first_name: 'Patricia',
